db9f32ff276cebf10bf98537162cf5cdb16a4918
이우주 <wown387@naver.com>
2024-07-14 20:59:20 +0900

    FIX: root

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import { AuthProvider } from './contexts/AuthContext';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import AdminLayout from './layouts/AdminLayout';
+import UsersPage from './pages/UserPage';
+import SettingsPage from './pages/SettingsPage';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/public" element={<HomePage />} />
+          <Route element={<PrivateRoute />}>
+            <Route element={<AdminLayout />}>
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/users" element={<UsersPage />} />
+              <Route path="/settings" element={<SettingsPage />} />
+            </Route>
+          </Route>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
-}
-
+};
 export default App;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,16 +2,13 @@
 import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import { NoneOptim } from '../components/NoneOptim';
-import { Optimim } from '../components/Optimim';
-
 const HomePage: React.FC = () => {
   const { logout } = useAuth();
   const navigate = useNavigate();
   return (
     <div>
-      <h2>Home Page</h2>      
-      <a  onClick={()=>{
+      <h2>Home Page</h2>
+      <a onClick={() => {
         navigate('/dashboard');
       }}>
         go to dashboard
